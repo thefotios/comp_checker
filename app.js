bbb248ece4d5756d377aa501b3ad91600679f14a
Fotios Lindiakos <flindiakos@shutterstock.com>
2014-06-19 18:04:44 -0400

    Split some stuff into it's own libs

--- app.js
+++ app.js
@@ -1,85 +1,25 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
 var async = require('async');
 var path = require('path');
 var _ = require('lodash');
 
-var Filequeue = require('filequeue');
-var fq = new Filequeue(200);
-
 var dot = require('./lib/dot');
-
-var cacheObj = {};
-var Glob = require('glob').Glob;
-
-// Find all files matching the pattern
-var findFiles = function( dir, pattern, cb){
-  var options = {
-    cwd: dir,
-    cache: cacheObj
-  };
-  // TODO: have glob expand file paths
-  Glob(pattern, options, function(err, matches){
-    async.map(matches, function(file, cb){
-      var filename = path.resolve(dir, file);
-      cb(null, filename);
-    }, function(err, results){
-      cb(results);
-    });
-  });
-};
-
-var findComps = function(filename, cb) {
-  // Read the file
-  fq.readFile(filename, 'utf8', function(err, data){
-    if(err){
-      // console.warn(err);
-    }
-    var comps = [];
-    parseComps(data, function(comp){
-      comps.push(comp);
-    });
-    cb(null, comps);
-  });
-};
-
-// This will find all single line comp statements
-// TODO: Make it work with multiline statements too
-var re = /\$m\->comp\((.*?)\)/g;
-var parseComps = function(lines, cb){
-  // TODO: do this async
-  while(matches = re.exec(lines)){
-    var comp = matches[1].replace(/['"\s]/g,'').split(',');
-    cb(comp[0]);
-  }
-};
-
-// This filters the hash and removes any empty values
-// TODO: It's late, there's gotta be a better way
-var createHash = function(keys, values) {
-  var hash = _.zipObject(keys, values);
-  var keep = _.reject(keys, function(key){
-    return hash[key].length === 0;
-  });
-  var hash2 = _.map(keep, function(key){
-    return hash[key];
-  });
-  var hash_keep = _.zipObject(keep, hash2);
-  return hash_keep;
-};
+var finder = require('./lib/finder');
+var comps = require('./lib/comps');
+var fn = require('./lib/fn');
 
 var globPattern = '**/*.+(mhtml|mh|md)';
 var dir = process.argv[2];
 
-findFiles(dir, globPattern, function(files){
+finder.find(dir, globPattern, function(files){
   async.map(files, function(file, cb){
-    findComps(file, cb);
+    comps.find(file, cb);
   }, function(err, results){
     files = _.map(files, function(a){
       return path.relative(dir, a);
     });
-    var hash = createHash(files, results);
+    var hash = fn.Hash(files, results);
 
     dot.create(hash);
   });
