b04c8f45ee6a45b637f186dff4f9b74ef1cd33ea
Fotios Lindiakos <flindiakos@shutterstock.com>
2014-07-17 18:31:55 -0400

    Break up mainfile a little bit

--- app.js
+++ app.js
@@ -12,16 +12,31 @@ var fn = require('./lib/fn');
 var globPattern = '**/*.+(mhtml|mh|md)';
 var dir = process.argv[2];
 
-finder.find(dir, globPattern, function(files){
-  async.map(files, function(file, cb){
-    comps.find(file, cb);
-  }, function(err, results){
-    files = _.map(files, function(a){
-      return path.relative(dir, a);
-    });
-    var hash = fn.Hash(files, results);
+// Store all found files
+var files;
+// This shows all uniq file names and which directories they exist in
+var uniqFiles = {};
 
-    dot.create(hash);
+var dumpResults = function(err, results){
+  var hash = fn.Hash(files, results);
+  // console.log(hash);
+
+  dot.create(hash);
+};
+
+finder.find(dir, globPattern, function(found){
+  // Store all file names relative to the current working directory
+  files = _.map(found, function(a){
+    var curFile = path.relative(dir, a);
+    var base = path.basename(curFile);
+
+    if(typeof(uniqFiles[base]) === 'undefined'){
+      uniqFiles[base] = [];
+    }
+    uniqFiles[base].push(curFile);
+    return curFile;
   });
+  async.map(found, function(file, cb){
+    comps.find(file, uniqFiles, cb);
+  }, dumpResults);
 });
-
