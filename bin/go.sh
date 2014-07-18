82b53472528421733250e9220d9cf584e817a35f
Fotios Lindiakos <flindiakos@shutterstock.com>
2014-07-18 01:43:29 -0400

    Attempt to filter nodes based on project

--- bin/go.sh
+++ bin/go.sh
@@ -1,7 +1,31 @@
 #!/usr/bin/env bash
 
-json='foo.json'
+base="foo"
 dot='foo.dot'
-node app.js $1 | perl bin/gen_file_tree.pl > $json;
-node make_graph.js $json > $dot;
-bin/make_graph.sh $dot $2;
+step=1
+
+# Create the initial json of all the data
+outfile="${base}.${step}.json"
+node app.js $1 | python -m json.tool > $outfile
+infile=$outfile
+
+# First pass
+((step++))
+outfile="${base}.${step}.json"
+cat $infile | perl bin/gen_file_tree.pl | python -m json.tool > $outfile
+infile=$outfile
+
+# Filter
+((step++))
+outfile="${base}.${step}.json"
+node make_graph.js $infile $2 | python -m json.tool > $outfile
+infile=$outfile
+
+# Rerun
+((step++))
+outfile="${base}.${step}.json"
+cat $infile | perl bin/gen_file_tree.pl | python -m json.tool > $outfile;
+infile=$outfile
+
+node make_graph.js $infile > $dot;
+bin/make_graph.sh $dot $3;
--- make_graph.js
+++ make_graph.js
@@ -11,4 +11,22 @@ var fn = require('./lib/fn');
 
 var json = require(process.argv[2]);
 
-dot.create(json);
+var filter = process.argv[3];
+
+// TODO: It's 1am, this is not optimal
+if(typeof(filter) !== 'undefined'){
+  var wanted_uuids = dot.filter(json, filter);
+
+  // Only select the nodes we care about
+  var nodes = _.invert(json.nodes);
+  var new_data = _.invert(_.zipObject(wanted_uuids, _.at(nodes, wanted_uuids)));
+  json.nodes = new_data;
+
+  // Only select the edges we care about
+  var edges = _.at(json.edgeList, wanted_uuids);
+  json.edgeList = _.zipObject(wanted_uuids, edges);
+
+  console.log(JSON.stringify(json));
+}else{
+  dot.create(json);
+}
