c431e275322ce6830d5e845e20d9185a8c1feb5b
Mike Crowl <mcrowl@shutterstock.com>
2014-07-17 20:29:41 -0400

    add recurse method (perl version)

---
+++ bin/gen_file_tree.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+#
+use strict;
+use warnings;
+use Getopt::Long;
+use File::Basename;
+use Data::Dumper;
+use Digest::MD5 qw(md5_hex);
+use JSON::XS;
+
+my @data;
+while (<>) {
+	chomp($_);
+	push @data, $_;
+}
+my $json_str = join("\n", @data);
+my $filelist_with_sums = JSON::XS::decode_json($json_str);
+my %file_tree;
+foreach my $pathname (keys %{$filelist_with_sums}) {
+	my @path_parts  = split(/\//, $pathname);
+	recurse_build_tree(\%file_tree, \@path_parts, $filelist_with_sums->{$pathname});
+}
+
+print JSON::XS::encode_json(\%file_tree);
+
+
+###########################################################################
+sub recurse_build_tree {
+	my ($hashref, $path, $id) = @_;
+
+	my $node;
+	return if(@$path == 0); # done recursing
+	if(!exists $hashref->{$path->[0]}) {
+		if(@$path == 1) {
+			if(!exists $hashref->{'.'}) {
+				$hashref->{'.'} = [];
+			}
+			#push @{$hashref->{'.'}}, $path->[0];
+			push @{$hashref->{'.'}}, $id;
+			shift @$path;
+			return recurse_build_tree($hashref->{'.'}, $path, $id);
+		} else {
+			$hashref->{$path->[0]} = {};
+			$node = shift @$path;
+			return recurse_build_tree($hashref->{$node}, $path, $id);
+		}
+	} else {
+		$node = shift @$path;
+		return recurse_build_tree($hashref->{$node}, $path, $id);
+	}
+}
+
+sub list_files {
+	my @file_list = @_;
+
+	foreach my $file (sort @file_list) {
+		print "$file\n";
+	}
+}
+
+
